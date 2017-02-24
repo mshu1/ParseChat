b32cb3da301bff1e29249e16b3eb855119bef211
Michelle <mshu1@jhu.edu>
2017-02-23 22:51:11 -0500

    first commit

---
+++ Parse Chat/MessageCell.swift
@@ -0,0 +1,24 @@
+//
+//  MessageCell.swift
+//  Parse Chat
+//
+//  Created by Michelle Shu on 2/23/17.
+//  Copyright © 2017 Michelle Shu. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
